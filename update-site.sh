b7acd62240e22e62891ac04a8dddebe55fe8483e
Johan Thelin <e8johan@gmail.com>
2020-07-22 22:00:55 +0200

    Support --no-images argument, to skip the conversion of images

--- update-site.sh
+++ update-site.sh
@@ -1,6 +1,21 @@
 #!/bin/sh
 
-./convert-images.sh
+images="yes"
+if [ "$#" -eq "1" ]; then
+    case "$1" in
+        "--no-images")
+            images="no"
+            ;;
+        *)
+            echo "Unknown argument '$1'"
+            exit 1
+            ;;
+    esac
+fi
+
+if [ "$images" = "yes" ]; then
+    ./convert-images.sh
+fi
 
 for s in source/*; do
     d=$(echo $s|sed 's/^source/build/')
